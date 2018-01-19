8e5cd86a619d1bf436a3e24c9da4c807dcf1209a
J. Adam Moore <DieLaughing@users.noreply.github.com>
2018-01-19 16:32:15 -0500

    Added redux-slider-monitor from npm

--- src/containers/DevTools/DevTools.js
+++ src/containers/DevTools/DevTools.js
@@ -2,10 +2,13 @@ import React from 'react';
 import { createDevTools } from 'redux-devtools';
 import LogMonitor from 'redux-devtools-log-monitor';
 import DockMonitor from 'redux-devtools-dock-monitor';
+import SliderMonitor from 'redux-slider-monitor';
 
 export default createDevTools(
   <DockMonitor toggleVisibilityKey="ctrl-H"
-               changePositionKey="ctrl-Q">
+               changePositionKey="ctrl-D"
+               changeMonitorKey="ctrl-M">
     <LogMonitor />
+    <SliderMonitor />
   </DockMonitor>
 );
